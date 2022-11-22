d18fe6cedc1137129055a26488aa076b8f7bfc83
Michalina <corshubbia@gmail.com>
2022-11-22 20:37:01 +0100

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth import authenticate, login
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def blog(request):
+    return render(request, 'bloghome.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def blogpost(request, slug):
+    return HttpResponse(f"You are viewing {slug}")
+
+def search(request):
+    return render(request, 'search.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        # Process the request if posted data are available
+        username = request.POST['username']
+        password = request.POST['password']
+        # Check username and password combination if correct
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            # Save session as cookie to login the user
+            login(request, user)
+            # Success, now let's login the user.
+            return render(request, 'account.html')
+        else:
+            # Incorrect credentials, let's throw an error to the screen.
+            return render(request, 'login.html', {'error_message': 'Incorrect username and / or password.'})
+    else:
+        # No post data availabe, let's just show the page to the user.
+        return render(request, 'login.html')
